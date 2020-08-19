c6a0db28eab6a42a038b9d4c5a5447a0ee3a4a15
deveshdatwani <datwanidevesh@gmail.com>
2020-08-19 18:26:28 +0530

    yo

--- heroku_app/create_world_map_trends.py
+++ heroku_app/create_world_map_trends.py
@@ -3,10 +3,11 @@ import itertools
 import json
 import requests
 import pandas as pd
-
+from find_trends import find_current_trends
 
 def load_map_with_trends():
 
+	find_current_trends()
 	m = folium.Map(tiles='Stamen Terrain',  zoom_start=3, location=[20.76, 79])
 	country_cordinates = pd.read_csv('trending_cities.csv')
 	tooltip = 'See Trends'
@@ -17,7 +18,7 @@ def load_map_with_trends():
 		try:
 			folium.Marker([lat[i], lon[i]], popup = '<i>This is {}<i>'.format(city), tooltip = tooltip).add_to(m)
 		except: pass
-	m.save('trends_on_the_map.html')
+	m.save('templates/trends_on_the_map.html')
 
 	return None
 
