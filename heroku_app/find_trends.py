576fbf8be05c7ad9ed2212bf3cbbd4bbd23c18ff
deveshdatwani <datwanidevesh@gmail.com>
2020-08-19 19:27:07 +0530

    removed tokens

--- heroku_app/find_trends.py
+++ heroku_app/find_trends.py
@@ -10,13 +10,14 @@ def find_current_trends():
 	url = 'https://api.twitter.com/1.1/trends/available.json'
 	bearer = os.environ['BEARER_TOKEN']
 	response = requests.get(url=url, headers = {'authorization': 'Bearer ' + bearer})
-	worldwide_trending = response.json()
+	response = response.json()
+	rending_cities = []
 
-	city_coordinates = pd.read_csv('worldcities.csv')
-	trending_woeid = [[trend['name'], trend['country'], trend['woeid']] for trend in worldwide_trending]
-	trending_woeid = pd.DataFrame(trending_woeid, columns = ['city', 'country', 'woeid'])
+	for trend in response:
+		trending_cities.append([trend['name'], trend['country'], trend['woeid']])
 
-	city_coordinates = city_coordinates[['city', 'lat', 'lng', 'country']]
-	trending_cities = trending_woeid.merge(city_coordinates, on=['country','city'])
+	trending_cities_df = pd.DataFrame(trending_cities, columns=['city','country','woeid'])
+	city_coordinates = pd.read('worldcities.csv')
+	city_coordinates = city_coordinates[['city','country','lat','lng']]
+	trending_cities_df = trending_cities_df.merge(city_coordinates,on=['country','city'])
 
-	trending_cities.to_csv('trending_cities.csv')
