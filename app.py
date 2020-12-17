800982cad9132bfd3f8d63da18bfc28a0a6934d0
Ubuntu <ubuntu@ip-172-31-41-4.ap-south-1.compute.internal>
2020-12-17 14:22:38 +0000

    backing up

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, url_for
+import pandas as pd
+import mysql.connector
+import folium
+from jinja2 import PackageLoader, select_autoescape, Environment, Template
+from apscheduler.schedulers.background import BackgroundScheduler
+from find_trends import find_trends
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(find_trends, 'interval', minutes=5)
+sched.start()
+
+@app.route('/', methods=('GET','POST'))
+def main_page():
+
+	if request.method == 'GET':
+
+		tooltip = 'See Trends'
+
+		connector = mysql.connector.connect(user='devesh', password='trendsonthemap', host='localhost', database='trends')
+		m = folium.Map(tiles='Stamen Terrain', min_zoom=3, zoom_start=3, location=[20.76, 79])
+
+		if connector.is_connected():
+			cursor = connector.cursor()
+			query = 'SELECT * FROM trendsincities'
+			cursor.execute(query)
+			response = cursor.fetchall()
+			cursor.close()
+			connector.close()
+
+			for i, city in enumerate(response):
+
+				try:
+
+					folium.Marker([round(city[4],2), round(city[5],2)], popup = '<i>><span style="font-weight:900; margin-left:30%;">{}</span><br><br>{}<i>'.format(city[1], '<br>'.join(city[6].split('-')[:10])), tooltip = tooltip).add_to(m)
+
+				except Exception as e: print(e)
+
+		final_display = m._repr_html_()
+
+		env = Environment( loader=PackageLoader('app', 'templates'), autoescape=select_autoescape(['html', 'xml']) )
+		template = env.get_template('trends.html')
+		map = template.render(map=final_display)
+
+	return map
+
---
+++ find_trends.py
@@ -0,0 +1,85 @@
+import os
+import requests
+import pandas as pd
+from time import sleep
+import mysql.connector
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+def find_trends():
+    bearer = 'AAAAAAAAAAAAAAAAAAAAAHuKIwEAAAAAQ5TCFup6cTU5Fh4GkEJxiOEGwmM%3DZM0q1OZ9beUM2GJS6WInCn6GzDgpwoiPnTz7eDEp914OOOXedI'
+    #bearer = 'AAAAAAAAAAAAAAAAAAAAAFpzGwEAAAAA5S13Nnr0miSgTbDczld3rhUUbgY%3DhP5gWNZjfBjIx0x9TfTSOFeEhs3wm4KoINLUr9MaoKntDEPyHw'
+    connector = mysql.connector.connect(user='devesh', password='trendsonthemap', host='localhost', database='trends')
+    print('connected to database')
+
+    if connector.is_connected():
+        cursor = connector.cursor(buffered=True)
+        query = 'SELECT * FROM iterator WHERE holder = "it_holder" '
+        cursor.execute(query)
+        response = cursor.fetchone()
+    i = response[1]
+
+    if i > 19:
+        response = 1
+    else:
+        response = response[1] + 1
+
+    print('Updated iterator {}'.format(response))
+    second_query = "UPDATE iterator SET it = {} WHERE holder = 'it_holder' ".format(response)
+    cursor.execute(second_query)
+    connector.commit()
+    third_query = 'SELECT * FROM trendsincities'
+    third_response = cursor.execute(third_query)
+    response = cursor.fetchall()
+    print('iterator token accessed and updated. Fetched trendingcities')
+
+    split_end = i * 20
+    split_start = split_end - 20
+    print('start and end {} - {}'.format(split_start, split_end))
+
+    trends = [x[6] for x in response]
+    woeids = [x[3] for x in response]
+    woeids_copy = woeids
+    trends_copy = trends
+    woeids = list(woeids[split_start:split_end])
+    trends = list(trends[split_start:split_end])
+    trends_in_woeids = []
+    url = 'https://api.twitter.com/1.1/trends/place.json'
+    l2 = []
+    response = []
+
+    if len(woeids) > 0:
+
+        for woeid in woeids:
+            param = {'id' : woeid}
+            r = requests.get(url = url, headers = {'authorization': 'Bearer ' + bearer}, params = param).json()
+            response.append(r)
+            print('acquiring trends for {}'.format(woeid))
+
+        for i in response:
+            l1 = []
+            print(i)
+            for j in i[0]['trends']:
+                l1.append(j['name'])
+            l2.append('-'.join(l1))
+
+        trends = l2
+        print('All trends acquired')
+        trends = tuple(trends)
+        cursor.close()
+        connector.close()
+        connector = mysql.connector.connect(user='devesh', password='trendsonthemap', host='localhost', database='trends')
+        cursor = connector.cursor(buffered=True)
+
+        for i, trend in enumerate(trends):
+            cursor.execute('UPDATE trendsincities SET trends = %s WHERE woeid = %s', (trend, woeids[i]))
+            print(trend)
+            connector.commit()
+
+        print('20 trends inserted')
+        cursor.close()
+        connector.close()
+
+    else:
+        pass
+
+    return None
